d4cfc56d0a0d09d75bafe62ce99f0484e58bc172
Francisco Valdés <ecodisonante@gmail.com>
2024-04-13 18:44:31 -0400

    Service y Repository de Autor

---
+++ src/main/java/com/fullstack/publicaciones/services/IAutorService.java
@@ -0,0 +1,19 @@
+package com.fullstack.publicaciones.services;
+
+import java.util.List;
+import java.util.Optional;
+import com.fullstack.publicaciones.model.Autor;
+
+public interface IAutorService {
+    
+    List<Autor> getAllAutors();
+
+    Optional<Autor> getAutorById(Long id);
+
+    Autor createAutor(Autor autor) throws Exception;
+
+    Autor updateAutor(Long id, Autor autor) throws Exception;
+
+    void deleteAutor(Long id) throws Exception;
+
+}
