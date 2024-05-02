d5e812e1dc64e6a11afe4e54415ab43f529053d6
Francisco Valdés <ecodisonante@gmail.com>
2024-05-01 23:24:56 -0400

    AutorController con hateoas

--- src/main/java/com/fullstack/publicaciones/controller/AutorController.java
+++ src/main/java/com/fullstack/publicaciones/controller/AutorController.java
@@ -2,8 +2,12 @@ package com.fullstack.publicaciones.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,14 +29,28 @@ import org.springframework.web.bind.annotation.PutMapping;
 public class AutorController {
 
     @Autowired
-    IAutorService _service;
+    IAutorService service;
 
+    private static final String ALL_AUTORES = "lista-autores";
+    
     @GetMapping
     public ResponseEntity<Object> getAutoresList() {
-        List<Autor> autores = _service.getAllAutors();
+        List<Autor> autores = service.getAllAutors();
 
-        if (!autores.isEmpty())
-            return ResponseEntity.ok(autores);
+        if (!autores.isEmpty()) {
+            var autoresResouce = autores.stream()
+                    .map(autor -> EntityModel.of(autor,
+                            WebMvcLinkBuilder
+                                    .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutor(autor.getId()))
+                                    .withSelfRel()))
+                    .collect(Collectors.toList());
+
+            WebMvcLinkBuilder linkTo = WebMvcLinkBuilder
+                    .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutoresList());
+
+            return ResponseEntity.ok(CollectionModel.of(autoresResouce, linkTo.withRel("publicaciones")));
+
+        }
         else
             return ResponseEntity.ok(new ResponseDTO(HttpStatus.OK.value(), "No hay autores ingresados"));
     }
@@ -40,13 +58,23 @@ public class AutorController {
     @GetMapping("/{id}")
     public ResponseEntity<Object> getAutor(@PathVariable Long id) {
         try {
-            Optional<Autor> autor = _service.getAutorById(id);
+            Optional<Autor> autor = service.getAutorById(id);
 
-            if (!autor.isPresent())
+            if (autor.isPresent()) {
+                var autorModel = EntityModel.of(autor.get(),
+                        WebMvcLinkBuilder
+                                .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutor(id))
+                                .withSelfRel(),
+                        WebMvcLinkBuilder
+                                .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutoresList())
+                                .withRel(ALL_AUTORES));
+
+                return ResponseEntity.ok(autorModel);
+
+            } else
                 return ResponseEntity.badRequest().body(
                         new ResponseDTO(HttpStatus.BAD_REQUEST.value(), "No existe el autor con el id " + id));
 
-            return ResponseEntity.ok(autor.get());
 
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new ResponseDTO(
@@ -62,24 +90,46 @@ public class AutorController {
                     HttpStatus.BAD_REQUEST.value(), "Debe indicar Nombre y Correo de Autor"));
 
         try {
-            return ResponseEntity.ok(_service.createAutor(autor));
+            var createResult = service.createAutor(autor);
+
+            var autorModel = EntityModel.of(createResult,
+                    WebMvcLinkBuilder
+                            .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutor(createResult.getId()))
+                            .withSelfRel(),
+                    WebMvcLinkBuilder
+                            .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutoresList())
+                            .withRel(ALL_AUTORES));
+
+            return ResponseEntity.ok(autorModel);
+
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ResponseDTO(
+            return ResponseEntity.internalServerError().body(new ResponseDTO(
                     HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> putMethodName(@PathVariable Long id, @RequestBody Autor autor) {
+    public ResponseEntity<Object> updateAutor(@PathVariable Long id, @RequestBody Autor autor) {
         if (autor.getNombre() == null || autor.getNombre().isBlank()
                 || autor.getCorreo() == null || autor.getCorreo().isBlank())
             return ResponseEntity.badRequest().body(new ResponseDTO(
                     HttpStatus.BAD_REQUEST.value(), "Debe indicar Nombre y Correo de Autor"));
 
         try {
-            return ResponseEntity.ok(_service.updateAutor(id, autor));
+            var updated = service.updateAutor(id, autor);
+
+            var autorModel = EntityModel.of(updated,
+                    WebMvcLinkBuilder
+                            .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutor(updated.getId()))
+                            .withSelfRel(),
+                    WebMvcLinkBuilder
+                            .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAutoresList())
+                            .withRel(ALL_AUTORES));
+
+            return ResponseEntity.ok(autorModel);
+
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ResponseDTO(
+            return ResponseEntity.internalServerError().body(new ResponseDTO(
                     HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
         }
     }
@@ -87,7 +137,7 @@ public class AutorController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteAutor(@PathVariable Long id) {
         try {
-            _service.deleteAutor(id);
+            service.deleteAutor(id);
             return ResponseEntity.ok(new ResponseDTO(HttpStatus.OK.value(), "Autor eliminado."));
 
         } catch (Exception e) {
